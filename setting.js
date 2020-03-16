d3818f491859db70008fa3c4d421c0ee4bc72f2e
xhemj <xhemj2680@163.com>
2020-03-16 17:50:19 +0800

    add setting.js.

---
+++ setting.js
@@ -0,0 +1,23 @@
+window.$docsify = {
+        name: 'Xhemj博客',
+        repo: '',
+        loadSidebar: true,
+        loadNavbar: true,
+        coverpage: true,
+        subMaxLevel: 2,
+        auto2top: true,
+        relativePath: false,
+        themeColor: '#c30aff',
+        mergeNavbar: true,
+        formatUpdated: '{YYYY}/{MM}/{DD} {HH}:{mm}',
+        externalLinkTarget: '_blank',
+        notFoundPage: true,
+        search: 'auto',
+        search: {
+            maxAge: 86400000,
+            paths: 'auto',
+            placeholder: '搜索...',
+            noData: '未找到结果，换个搜索词试试？',
+            namespace: 'XhemjBlog',
+        },
+    }
