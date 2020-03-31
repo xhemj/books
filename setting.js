45f23560a787b87945b3841e4d159846d6fddb3b
xhemj <xhemj2680@163.com>
2020-03-31 19:30:51 +0800

    update setting.js.

--- setting.js
+++ setting.js
@@ -43,7 +43,7 @@ window.$docsify = {
          footer: {
             copy: '<strong>© 2020 Xhemj</strong>&nbsp;|&nbsp;<a href="/books" target="_blank">官网</a>&nbsp|&nbsp;<a href="https://xhemj.gitee.io/oneQRCodeforPay/" target="_blank">赞助本网站</a>&nbsp|&nbsp;<a href="https://gitee.com/xhemj/books/" target="_blank">Gitee</a>&nbsp|&nbsp;<a href="mailto:xhemj2680@163.com" target="_blank">联系我</a></br><img src="https://xhemj.gitee.io/logo.png" width="50" height="50">',
             auth: '<strong>BY: X H E M J</strong></br><strong>"这只是Xhemj的文档中心而已，也没有什么别的！"</strong><div id="cc-myssl-id"><a href="https://myssl.com/xhemj.gitee.io?from=mysslid" target="_blank"><img src="https://static.myssl.com/res/images/myssl-id3.png" alt="" style="max-height:50px;display:block;margin:0 auto"></a></br></div>',
-            pre: '<div id="social-share" style="text-align: center;">',
+            pre: '<hr>',
             style:'text-align: center;'
         },
         remoteMarkdown: {
