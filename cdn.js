8d053042a495db6a520318fa825dd42979223074
xhemj <55266496+xhemj@users.noreply.github.com>
2020-04-29 15:30:20 +0800

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -41,5 +41,9 @@ document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/827652549\/docsify
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-pagination.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/npm\/docsify-copy-code\"><\/script>");
 }
+//通用代码高亮
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-markdown.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-css.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-javascript.min.js\"><\/script>");
 //备用cdn库
 //<script src="https://cdn.bootcdn.net/ajax/libs/docsify/4.11.2/docsify.js"></script>
