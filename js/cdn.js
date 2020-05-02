d6932668f457e3b5afe137d1e541d3bfb9b3ba8b
xhemj <55266496+xhemj@users.noreply.github.com>
2020-05-02 11:27:31 +0800

    Update cdn.js

--- js/cdn.js
+++ js/cdn.js
@@ -1,7 +1,7 @@
 /*
 Docsify CDN
 */
-var cdnuse = "3"
+var cdnuse = "5"
 if(cdnuse == "1"){
 //使用Jsdelivr
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/npm\/docsify\/lib\/docsify.min.js\"><\/script>");
@@ -14,6 +14,10 @@ document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.githu
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-remote-markdown.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/npm\/docsify\/lib\/plugins\/external-script.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/827652549\/docsify-count@master\/dist\/countable.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-markdown.min.js\"><\/script>");
+//document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-css.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-javascript.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-bash.min.js\"><\/script>");
 }
 if(cdnuse == "2"){
 //尽量使用CDNJS+其它Jsdelivr
@@ -27,10 +31,18 @@ document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.githu
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/827652549\/docsify-count@master\/dist\/countable.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-pagination.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/npm\/docsify-copy-code\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-markdown.min.js\"><\/script>");
+//document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-css.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-javascript.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-bash.min.js\"><\/script>");
 }
 if(cdnuse == "3"){
 //使用Jsdelivr压缩合并功能
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/combine\/npm\/docsify\/lib\/docsify.min.js,npm\/docsify\/lib\/plugins\/search.min.js,npm\/docsify\/lib\/plugins\/zoom-image.min.js,npm\/docsify-copy-code,npm\/docsify\/lib\/plugins\/emoji.min.js,gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-pagination.min.js,gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-footer-enh.min.js,gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-remote-markdown.min.js,npm\/docsify\/lib\/plugins\/external-script.min.js,gh\/827652549\/docsify-count@master\/dist\/countable.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-markdown.min.js\"><\/script>");
+//document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-css.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-javascript.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-bash.min.js\"><\/script>");
 }
 if(cdnuse == "4"){
 //尽量使用loli.net+其它Jsdelivr
@@ -44,6 +56,10 @@ document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.githu
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/827652549\/docsify-count@master\/dist\/countable.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-pagination.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/npm\/docsify-copy-code\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-markdown.min.js\"><\/script>");
+//document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-css.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-javascript.min.js\"><\/script>");
+document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-bash.min.js\"><\/script>");
 }
 if(cdnuse == "5"){
 //尽量使用Staticfile+其它Jsdelivr
@@ -57,12 +73,12 @@ document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.githu
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-footer-enh.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.github.io@master\/backup\/js\/docsify-remote-markdown.min.js\"><\/script>");
 document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/827652549\/docsify-count@master\/dist\/countable.min.js\"><\/script>");
-}
-//通用其它功能js
 document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-markdown.min.js\"><\/script>");
 //document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-css.min.js\"><\/script>");
 document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-javascript.min.js\"><\/script>");
 document.write("<script src=\"\/\/cdn.jsdelivr.net\/npm\/prismjs\/components\/prism-bash.min.js\"><\/script>");
+}
+//通用其它功能js
 //document.write("<script src=\"https:\/\/cdn.jsdelivr.net\/gh\/xhemj\/xhemj.github.io@master\/backup\/js\/instantpage.js\" type=\"module\" defer ><\/script>");
 
 //备用cdn库
