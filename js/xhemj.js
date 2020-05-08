ede6fe61934b2a7e2ee621ae9588cf58ccecdc4c
xhemj <55266496+xhemj@users.noreply.github.com>
2020-05-08 15:49:55 +0800

    Update xhemj.js

--- js/xhemj.js
+++ js/xhemj.js
@@ -2,6 +2,6 @@
  * Xhemj Console    *
  *--------------------*/
 xhemjconsole()
-setInterval(xhemjconsole, 10000);
+setInterval(xhemjconsole, 5000);
 function xhemjconsole() {
 console.clear();console.log("%c ------------⚠--------------\n%c  Power BY ⚡ %c xhemj %c ⚡\n%c \t ©2020 xhemj\n%c ---------------------------\n%c💬"+location.href,"font-size:20px;font-weight:900;","font-size:20px;font-weight:900;","font-size:20px;color:#C30AFF;font-weight:900;","font-size:20px;font-weight:900;","color:#867676;font-size:20px;font-weight:900;","font-size:20px;font-weight:900;","font-weight:100;")}
