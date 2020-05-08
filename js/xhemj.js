b3440ea295d5e760f9a9684c20ee49a1b4b487da
xhemj <55266496+xhemj@users.noreply.github.com>
2020-05-08 09:02:38 +0800

    Create xhemj.js

---
+++ js/xhemj.js
@@ -0,0 +1,8 @@
+/*----------*
+ * Console  *
+ *----------*/
+console.log("%c --------------------------","font-size:20px;font-weight:900;");
+console.log("%c  Power BY ⚡ %c xhemj %c ⚡","font-size:20px;font-weight:900;","font-size:20px;color:#C30AFF;font-weight:900;","font-size:20px;font-weight:900;");
+console.log("%c \t ©2020 xhemj","color:#867676;font-size:20px;font-weight:900;");
+console.log("%c --------------------------","font-size:20px;font-weight:900;");
+console.log(location.href);
