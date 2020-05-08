7450f9dec747ca9d76466a79b804d568e9010309
xhemj <55266496+xhemj@users.noreply.github.com>
2020-05-08 01:55:23 +0000

    Update xhemj.js

--- js/xhemj.js
+++ js/xhemj.js
@@ -5,4 +5,4 @@ console.log("%c ------------⚠--------------","font-size:20px;font-weight:900;"
 console.log("%c  Power BY ⚡ %c xhemj %c ⚡","font-size:20px;font-weight:900;","font-size:20px;color:#C30AFF;font-weight:900;","font-size:20px;font-weight:900;");
 console.log("%c \t ©2020 xhemj","color:#867676;font-size:20px;font-weight:900;");
 console.log("%c ---------------------------","font-size:20px;font-weight:900;");
-console.log("🌎链接："+location.href+"🖥");
+console.log("🌎链接："+location.href+"💬");
